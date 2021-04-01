607c59627b40830473049ea10eb91d7d02b2a8dd
maryko-makk <79507695+maryko-makk@users.noreply.github.com>
2021-04-01 11:29:09 +0300

    feat: add alt attribute to images according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ var createNewTaskElement=function(taskString){
 
   deleteButton.className="delete";
   deleteButtonImg.src='./remove.svg';
+  deleteButtonImg.setAttribute("alt", "delete button");
   deleteButton.appendChild(deleteButtonImg);
 
   //and appending.
